40efcb4ec88bd7104753fecc97cf1b14c64e60bb
Evgeny-Chernitsin <Evgeny-Chernitsin@github.com>
2023-03-14 13:38:27 +0500

    fix: Исправил ошибки Ревью

---
+++ scripts /index.js
@@ -0,0 +1,36 @@
+
+let containerPopup = document.querySelector('.popup');
+let openButton = document.querySelector('.profile__button-edit');
+let closeButton = containerPopup.querySelector('.popup__botton-close');
+
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__job');
+
+
+
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__job');
+
+function popupOpen() {
+  containerPopup.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
+
+function popupClose() {
+  containerPopup.classList.remove('popup_opened');
+
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+
+  popupClose()
+}
+
+containerPopup.addEventListener('submit', handleFormSubmit);
+openButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
